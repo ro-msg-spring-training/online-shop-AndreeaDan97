dd5b78ca1b815f6b653214ab763fef3e0a23aae6
AndreeaDan97 <andreea.dan97@yahoo.com>
2021-04-20 14:28:22 +0300

    removed some unused imports

--- shop/src/main/java/ro/msg/learning/shop/controllers/OrderController.java
+++ shop/src/main/java/ro/msg/learning/shop/controllers/OrderController.java
@@ -2,19 +2,13 @@ package ro.msg.learning.shop.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ro.msg.learning.shop.dtos.OrderDetailsDto;
 import ro.msg.learning.shop.dtos.OrderDto;
-import ro.msg.learning.shop.dtos.ProductDto;
-import ro.msg.learning.shop.entities.Order;
 import ro.msg.learning.shop.entities.OrderDetails;
 import ro.msg.learning.shop.services.OrderService;
 
@@ -27,7 +21,6 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
-
 
     @PostMapping(value="/insert")
     public ResponseEntity<List<OrderDetails>> addOrder(@RequestBody OrderDto orderDto) {
